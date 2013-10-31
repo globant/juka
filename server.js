7a065b7dd6b5a997b48d53e763654a9ec2a4cd1b
Enrique Alejandro Bakun <enrique.bakun@glb7311.globant.com>
2013-10-31 19:19:29 -0300

    Sebastian Scatularo - added fix for serve static files

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var SampleApp = function() {
         }
 
         //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
+        self.zcache['index.html'] = fs.readFileSync('./front_end/index.html');
     };
 
 
@@ -114,6 +114,8 @@ var SampleApp = function() {
     self.initializeServer = function() {
         self.createRoutes();
         self.app = express.createServer();
+
+	self.app.use("/", express.static(__dirname + '/front_end'));
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
