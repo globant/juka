677810312a12268f1065dac1ccba42b8621fdaba
fgobenitez <facundo.benitez@globant.com>
2013-11-21 18:55:54 -0300

    Included box2d into cocos2d.

---
+++ front_end/src/game/actors/tankSprite.js
@@ -0,0 +1,185 @@
+// http://www.cocos2d-iphone.org/forums/topic/subclass-ccsprite-vs-model-class-best-practice/
+// http://www.cocos2d-x.org/html5-samples/samples/games/MoonWarriors/index.html //
+// 
+var MAX_STEER_ANGLE = Math.PI/3;
+var STEER_SPEED = 1.5;
+var HORSEPOWERS = 40
+
+var TankSprite = cc.Sprite.extend({
+
+    maxSteerAngle:0,
+    steerSpeed:0,
+    sidewaysFrictionForce:10,
+    horsePower:40,
+    carStartingPos:null,
+    currentRotation:0,
+    engineSpeed:0,
+    steeringAngle:0,
+    b:{},
+
+   ctor:function(){
+      this._super();
+      this.initWithFile("resources/images/tank.png");
+      this._initBox2DBody(box2dManager.world);
+   },
+
+   _initBox2DBody:function(){
+
+      this.maxSteerAngle= Math.PI/3;
+      this.steerSpeed=1.5;
+      this.sidewaysFrictionForce = 10;
+      this.horsePower = 40;
+      this.carStartingPos = new b2Vec2(10,10);
+    
+      var leftRearWheelPosition = new b2Vec2(-1.5,1.90);
+      var rightRearWheelPosition = new b2Vec2(1.5,1.9);
+      var leftFrontWheelPosition = new b2Vec2(-1.5,-1.9);
+      var rightFrontWheelPosition = new b2Vec2(1.5,-1.9);
+
+      var world = box2dManager.world;
+      var fixDef = new b2FixtureDef();
+
+      // define our body
+      var bodyDef = new b2BodyDef();
+      bodyDef.userData = this;
+      bodyDef.linearDamping = 1;
+      bodyDef.angularDamping = 1;
+      bodyDef.position = this.carStartingPos.Copy();
+      fixDef.density = 1;
+      fixDef.shape = new b2PolygonShape();
+      fixDef.shape.SetAsBox(1.5, 2.5);
+      bodyDef.type = b2Body.b2_dynamicBody;
+      this.b.body = world.CreateBody(bodyDef);
+      this.b.body.CreateFixture(fixDef);
+
+      var leftWheelDef = new b2BodyDef();
+      leftWheelDef.position = this.carStartingPos.Copy();
+      leftWheelDef.position.Add(leftFrontWheelPosition);
+      fixDef.shape =  new b2PolygonShape();
+      fixDef.shape.SetAsBox(0.2,0.5);
+      fixDef.density = 1;
+      leftWheelDef.type = b2Body.b2_dynamicBody;
+      this.b.leftWheel = world.CreateBody(leftWheelDef);
+      this.b.leftWheel.CreateFixture(fixDef);
+
+      var rightWheelDef = new b2BodyDef();
+      rightWheelDef.position = this.carStartingPos.Copy();
+      rightWheelDef.position.Add(rightFrontWheelPosition);
+      fixDef.shape = new b2PolygonShape();
+      fixDef.shape.SetAsBox(0.2,0.5);
+      fixDef.density = 1;
+      rightWheelDef.type = b2Body.b2_dynamicBody;
+      this.b.rightWheel = world.CreateBody(rightWheelDef);
+      this.b.rightWheel.CreateFixture(fixDef);
+
+      var leftRearWheelDef = new b2BodyDef();
+      leftRearWheelDef.position = this.carStartingPos.Copy();
+      leftRearWheelDef.position.Add(leftRearWheelPosition);
+      fixDef.shape = new b2PolygonShape();
+      fixDef.shape.SetAsBox(0.2,0.5);
+      fixDef.density = 1;
+      leftRearWheelDef.type = b2Body.b2_dynamicBody;
+      this.b.leftRearWheel = world.CreateBody(leftRearWheelDef);
+      this.b.leftRearWheel.CreateFixture(fixDef);
+   
+      var rightRearWheelDef = new b2BodyDef();
+      rightRearWheelDef.position = this.carStartingPos.Copy();
+      rightRearWheelDef.position.Add(rightRearWheelPosition);
+      fixDef.shape = new b2PolygonShape();
+      fixDef.shape.SetAsBox(0.2,0.5);
+      fixDef.density = 1;
+      rightRearWheelDef.type = b2Body.b2_dynamicBody;
+      this.b.rightRearWheel = world.CreateBody(rightRearWheelDef);
+      this.b.rightRearWheel.CreateFixture(fixDef);
+      
+      var leftJointDef = new b2RevoluteJointDef();
+      leftJointDef.Initialize(this.b.body, this.b.leftWheel, this.b.leftWheel.GetWorldCenter());
+      leftJointDef.enableMotor = true;
+      leftJointDef.maxMotorTorque = 100;
+      this.b.leftJoint = world.CreateJoint(leftJointDef);
+      
+      var rightJointDef = new b2RevoluteJointDef();
+      rightJointDef.Initialize(this.b.body, this.b.rightWheel, this.b.rightWheel.GetWorldCenter());
+      rightJointDef.enableMotor = true;
+      rightJointDef.maxMotorTorque = 100;
+      this.b.rightJoint = world.CreateJoint(rightJointDef);
+        
+      var leftRearJointDef = new b2PrismaticJointDef();
+      leftRearJointDef.Initialize(this.b.body, this.b.leftRearWheel, this.b.leftRearWheel.GetWorldCenter(), new b2Vec2(1,0));
+      leftRearJointDef.enableLimit = true;
+      leftRearJointDef.lowerTranslation = leftRearJointDef.upperTranslation = 0;
+      this.b.leftRearJoint = world.CreateJoint(leftRearJointDef);
+
+      var rightRearJointDef = new b2PrismaticJointDef();
+      rightRearJointDef.Initialize(this.b.body, this.b.rightRearWheel, this.b.rightRearWheel.GetWorldCenter(), new b2Vec2(1,0));
+      rightRearJointDef.enableLimit = true;
+      rightRearJointDef.lowerTranslation = rightRearJointDef.upperTranslation = 0;
+      this.b.rightRearJoint = world.CreateJoint(rightRearJointDef);
+
+   },
+   update:function(){
+
+
+    //This function applies a "friction" in a direction orthogonal to the body's axis.
+    function killOrthogonalVelocity(targetBody){
+      var localPoint = new b2Vec2(0,0);
+      var velocity = targetBody.GetLinearVelocityFromLocalPoint(localPoint);
+      var sidewaysAxis = targetBody.GetTransform().R.col2.Copy();
+      sidewaysAxis.Multiply(b2Math.b2Dot(velocity,sidewaysAxis));
+      targetBody.SetLinearVelocity(sidewaysAxis);//targetBody.GetWorldPoint(localPoint));        
+    }
+
+    b2Math.b2Dot = function(a, b)
+    {
+        return a.x * b.x + a.y * b.y;
+    };
+
+    //Driving
+    var ldirection = this.b.leftWheel.GetTransform().R.col2.Copy();
+    ldirection.Multiply(this.engineSpeed);
+    var rdirection = this.b.rightWheel.GetTransform().R.col2.Copy();
+    rdirection.Multiply(this.engineSpeed);
+    this.b.leftWheel.ApplyForce(ldirection, this.b.leftWheel.GetPosition());
+    this.b.rightWheel.ApplyForce(rdirection, this.b.rightWheel.GetPosition());
+
+    //Steering
+    var mspeed;
+    mspeed = this.steeringAngle - this.b.leftJoint.GetJointAngle();
+    this.b.leftJoint.SetMotorSpeed(mspeed * this.steerSpeed);
+    mspeed = this.steeringAngle - this.b.rightJoint.GetJointAngle();
+    this.b.rightJoint.SetMotorSpeed(mspeed * this.steerSpeed);
+
+
+    killOrthogonalVelocity(this.b.leftWheel);
+    killOrthogonalVelocity(this.b.rightWheel);
+    killOrthogonalVelocity(this.b.leftRearWheel);
+    killOrthogonalVelocity(this.b.rightRearWheel);
+
+
+    //This function applies a "friction" in a direction orthogonal to the body's axis.
+    function killOrthogonalVelocity(targetBody){
+      var localPoint = new b2Vec2(0,0);
+      var velocity = targetBody.GetLinearVelocityFromLocalPoint(localPoint);
+      var sidewaysAxis = targetBody.GetTransform().R.col2.Copy();
+      sidewaysAxis.Multiply(b2Math.b2Dot(velocity,sidewaysAxis));
+      targetBody.SetLinearVelocity(sidewaysAxis);
+    }
+
+   },
+   handleKey:function(e){
+      
+    if (e === cc.KEY.left){
+      this.steeringAngle = MAX_STEER_ANGLE;
+    }else if(e === cc.KEY.right){
+      this. steeringAngle = -MAX_STEER_ANGLE;
+    }else if(e === cc.KEY.up){
+      this.engineSpeed = HORSEPOWERS;
+    }else if(e === cc.KEY.down){
+      this.engineSpeed = -HORSEPOWERS;
+    }
+
+  }
+   
+});
+
+
---
+++ front_end/src/libs/globant/box2dManager.js
@@ -0,0 +1,98 @@
+ // bunch of variables required for various box2d objects
+
+var b2Vec2,b2AABB,b2BodyDef,b2Body,b2FixtureDef,b2Fixture,b2World,b2PolygonShape, 
+b2CircleShape,b2DebugDraw,b2RevoluteJointDef,b2RevoluteJoint,b2PrismaticJointDef,b2Math;
+
+var context;
+
+var box2dManager = {
+
+        canvas:null,
+        context:null,
+        screenW:0,
+        screenH:0,
+        world:null,
+        drawDebug:false,
+        scale:20.0,
+        /**
+         * Inits Box2d variables and initializes world.
+         * @param {String} canvasId the id of the canvas object.
+         * @return {b2World} Initiated world.
+         */
+        initWorld:function(canvasId){
+
+            //Init globalvariables.
+            b2Vec2 = Box2D.Common.Math.b2Vec2;
+            b2AABB = Box2D.Collision.b2AABB;
+            b2BodyDef = Box2D.Dynamics.b2BodyDef;
+            b2Body = Box2D.Dynamics.b2Body;
+            b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+            b2Fixture = Box2D.Dynamics.b2Fixture;
+            b2World = Box2D.Dynamics.b2World;
+            b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+            b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+            b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+            b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef;
+            b2RevoluteJoint =  Box2D.Dynamics.Joints.b2RevoluteJoint;
+            b2PrismaticJointDef = Box2D.Dynamics.Joints.b2PrismaticJointDef;
+            b2Math = Box2D.Common.Math.b2Math;
+
+            this.canvas = document.getElementById(canvasId);
+            
+            this.screenW = this.canvas.width, screenH = this.canvas.height;
+            this.world = new b2World(new b2Vec2(0, 0) ,  true );
+            this.scale = 20.0;
+
+            // Debug options that aren't working...
+            /*
+            this.context = this.canvas.getContext("webgl"); 
+            if (this.context == null){
+                    this.context = this.canvas.getContext("2d"); 
+            };
+            if (this.context ==null){
+                console.log("Couldn't get canvas context, debug draws won't be shown.");
+            };
+
+            
+            var debugDraw = new b2DebugDraw();
+            debugDraw.SetSprite(this.context);
+            debugDraw.SetDrawScale(this.scale);
+            debugDraw.SetFillAlpha(1);
+            debugDraw.SetLineThickness(1.0);
+            debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit | b2DebugDraw.e_centerOfMassBit);
+            this.world.SetDebugDraw(debugDraw);
+            */
+            return this.world;
+
+        },
+        init:function(){
+                console.log("hola");
+        },
+        update:function(){
+            this.world.Step(1 / 60, 3, 3); 
+            //this.world.DrawDebugData();
+            this.world.ClearForces();
+
+            //Iterate over the bodies in the physics world
+            for (var b = this.world.GetBodyList(); b; b = b.GetNext()) {
+                if (b.GetUserData() != null) {
+                    //Synchronize the AtlasSprites position and rotation with the corresponding body
+                    var myActor = b.GetUserData();
+                    myActor.setPosition(cc.p(b.GetPosition().x * PTM_RATIO, b.GetPosition().y * PTM_RATIO));
+                    myActor.setRotation(-1 * cc.RADIANS_TO_DEGREES(b.GetAngle()));
+                    //console.log(b.GetAngle());
+                }
+            }
+        }
+        
+};
+
+
+
+
+
+
+
+
+
+
