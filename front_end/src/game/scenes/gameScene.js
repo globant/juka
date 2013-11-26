89bdd8841d8ef64e614ce5184b298cd662cbc5fc
Lenscak José Francisco <malguzt@gmail.com>
2013-11-26 18:06:28 -0300

    Testing statics objects

--- front_end/src/game/scenes/gameScene.js
+++ front_end/src/game/scenes/gameScene.js
@@ -1,80 +1,80 @@
 var GameScene = cc.Scene.extend({
-    
-        onEnter:function(){
-            this._super();
-            var layer = new GameLayer();
-            layer.init();
-            this.addChild(layer);
-        }
-    
-    }
+  onEnter: function() {
+    this._super();
+    var layer = new GameLayer();
+    layer.init();
+    this.addChild(layer);
+  }
+
+}
 );
 
 var GameLayer = cc.Layer.extend({
-    _playerSprite:null,
-    init:function () {
-        this._super();
-        this.setKeyboardEnabled(true);
-        
-        var tileMap = new TileMeadow();
-        var objectGroup = tileMap.getObjectGroup("Objects");
-        var spawnPoint = objectGroup.objectNamed("player");
+  _playerSprite: null,
+  init: function() {
+    this._super();
+    this.setKeyboardEnabled(true);
 
-        // var mainThinks = tileMap.getObjectGroup("MainThinks");
-        // mainThinks.setVertexZ(10);
-        // var baseMap = tileMap.getObjectGroup("Base");
-        // baseMap.setVertexZ(20);
-      
-        this._playerSprite = new TankSprite();
-        this._playerSprite.setPosition(spawnPoint.x, spawnPoint.y);
-        this.scheduleUpdate();
-        this.schedule(this.update);
-        
-        tileMap.addChild( this._playerSprite);
-        
-        this.addChild(tileMap);
-        // this.addChild(mainThinks);
-        // this.addChild(baseMap);
-        this.setViewPointCenter( this._playerSprite.getPosition());
-        
-        return true;
-    },
-    setViewPointCenter:function(position){
-        var winSize = cc.Director.getInstance().getWinSize();
-        var x = Math.max(position.x, winSize.width/2);
-        var y = Math.max(position.y, winSize.height/2);
-        var actualPosition = cc.p(x, y);
-        var centerOfView = cc.p(winSize.width/2, winSize.height/2);
-        var viewPoint = cppSub(centerOfView, actualPosition);
-        this.setPosition(viewPoint);
-    },
-    update:function(dt){
-        box2dManager.update();        
-        if (typeof this._playerSprite != "undefined"){
-             this.setViewPointCenter( this._playerSprite.getPosition());
-         };
-        this._playerSprite.update();
-            
-    },
-    onKeyDown:function(e){
-        this._playerSprite.handleKey(e);
-    },
-    onKeyUp:function(e){
-      this._playerSprite.onKeyUp(e);
+    var tileMap = new TileMeadow();
+    var objectGroup = tileMap.getObjectGroup("Objects");
+    var spawnPoint = objectGroup.objectNamed("player");
+
+    // var mainThinks = tileMap.getObjectGroup("MainThinks");
+    // mainThinks.setVertexZ(10);
+    // var baseMap = tileMap.getObjectGroup("Base");
+    // baseMap.setVertexZ(20);
+
+    this._playerSprite = new TankSprite();
+    this._playerSprite.setPosition(spawnPoint.x, spawnPoint.y);
+    this.scheduleUpdate();
+    this.schedule(this.update);
+
+    tileMap.addChild(this._playerSprite);
+
+    this.addChild(tileMap);
+    // this.addChild(mainThinks);
+    // this.addChild(baseMap);
+    this.setViewPointCenter(this._playerSprite.getPosition());
+
+    return true;
+  },
+  setViewPointCenter: function(position) {
+    var winSize = cc.Director.getInstance().getWinSize();
+    var x = Math.max(position.x, winSize.width / 2);
+    var y = Math.max(position.y, winSize.height / 2);
+    var actualPosition = cc.p(x, y);
+    var centerOfView = cc.p(winSize.width / 2, winSize.height / 2);
+    var viewPoint = cppSub(centerOfView, actualPosition);
+    this.setPosition(viewPoint);
+  },
+  update: function(dt) {
+    box2dManager.update();
+    if (typeof this._playerSprite != "undefined") {
+      this.setViewPointCenter(this._playerSprite.getPosition());
     }
-            
+    ;
+    this._playerSprite.update();
+
+  },
+  onKeyDown: function(e) {
+    this._playerSprite.handleKey(e);
+  },
+  onKeyUp: function(e) {
+    this._playerSprite.onKeyUp(e);
+  }
+
 });
 
-function cppSub(pointA, pointB){
-    return cc.p(pointA.x -pointB.x,pointA.y- pointB.y);
+function cppSub(pointA, pointB) {
+  return cc.p(pointA.x - pointB.x, pointA.y - pointB.y);
 }
 
 var TileMeadow = cc.TMXTiledMap.extend({
-    ctor: function(){
-        this._super();
-        this.init();
-        this.initWithTMXFile("resources/images/TileMap.tmx");
-    }
-    
+  ctor: function() {
+    this._super();
+    this.init();
+    this.initWithTMXFile("resources/images/TileMap.tmx");
+  }
+
 });
 
